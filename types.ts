6e3cc942fb6cc9be337114a30576aa0a0af331d7
oxy-Op <mdrehaan7766@gmail.com>
2023-10-16 00:41:33 +0530

    fixed some errors

--- types.ts
+++ types.ts
@@ -3,7 +3,7 @@ import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
 import { Server, Member, Profile } from "@prisma/client"
 
-export type ServerWithMembersWithProfiles = Server & {
+export type ServerW = Server & {
   members: (Member & { profile: Profile })[];
 };
 

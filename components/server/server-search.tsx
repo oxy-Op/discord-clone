7d1359969d27f72c5c3ea1eeeac0f5ffd3a9acab
oxy-Op <mdrehaan7766@gmail.com>
2023-10-16 23:45:53 +0530

    fixed search params null

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -51,11 +51,11 @@ export const SearchServer = ({ data }: ServerSearchProps) => {
   }) => {
     setOpen(false);
     if (type === "member") {
-      return router.push(`/servers/${params.serverId}/conversations/${id}`);
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
     }
 
     if (type === "channel") {
-      return router.push(`/servers/${params.serverId}/channels/${id}`);
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
     }
   };
 
